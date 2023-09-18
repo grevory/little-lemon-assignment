e3532716dff0a040bd2db2091b654501e3d1e466
Gregory Pike <greg@gregpike.ca>
2023-09-18 15:43:47 -0230

    update structure

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Nav } from "./Nav";
+
+export function Header(children) {
+	return (
+		<header>
+			<img src="" alt="Little Lemon" />
+			<Nav></Nav>
+		</header>
+	);
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+export function Nav(children) {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<a href="">Home</a>
+				</li>
+				<li>
+					<a href="">About</a>
+				</li>
+				<li>
+					<a href="">Menu</a>
+				</li>
+				<li>
+					<a href="">Reservation</a>
+				</li>
+				<li>
+					<a href="">Order Online</a>
+				</li>
+				<li>
+					<a href="">Login</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
